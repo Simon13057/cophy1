c47e0556fcc4c099456aa0fae2e1d404afdffd50
Simon <sim.thamm@gmail.com>
2021-05-15 13:29:28 +0200

    teilaufgabe c/d

--- resonanz.py
+++ resonanz.py
@@ -3,7 +3,7 @@ import numpy as np
 import math
 
 def verletMax(init, force):
-    h = init[1]
+    h = init[1] #Bedeutung siehe unten
     omega = init[2]
     xp = init[3]
     vp = init[4]
@@ -12,8 +12,7 @@ def verletMax(init, force):
     f = force[1]
 
     max = 0
-    end = init[0]/2. #change first cond
-    #print(end)
+    end = init[0]/2. #Ende Einschwingvorgang
     x = xp + h * vp
 
     t = h
@@ -46,7 +45,7 @@ def callVerletMax(init, force):
     while omega <= maxOmega:
         res[0][index] = omega
         force[1] = omega
-        init[0] = 65/(omega*init[5])  #Variabel
+        init[0] = 150/(omega*init[5])  #Anpassen -- entspricht Laufzeit
         max = verletMax(init, force)
         res[1][index] = max
         omega += h
@@ -98,8 +97,8 @@ def plotting(toPlot, label):
 
     plt.show()
 
-def teilD(init, force, filename):
-    d = 0.5
+def teilD(init, force, filename): #für 3d plot 
+    d = 0.5 
     h = 0.1
     with open(filename, "w") as f:
         while d <= 1.5:
@@ -111,46 +110,40 @@ def teilD(init, force, filename):
                 index += 1
             f.write("\n")
             d += h
-# Main
-initV1 = np.array([100,0.1,1,1,0,0.6])
-initV2 = np.array([100,0.1,1,1,0,0.8])
-initV3 = np.array([100,0.1,1,1,0,1])
 
-init = np.array([100,0.5,1,1,0,0.4])
+# Main --- PLOTTING
+
+#Bedeutung der Werte nach Reihenfolge: Laufzeit, Schrittweite, Omega, Startwert x, Startwert v, Dämpfung d
+#Mit anregender kraft. Werte nach Reihenfolge: Amplitude A, Frequenz F
+
+initV1 = np.array([100,0.01,1,1,0,1])
+initV2 = np.array([100,0.01,1,1,0,3])
+
+init = np.array([100,0.01,1,1,0,2])
 force = np.array([1, 0.5])
 
-teilD(init, force, "teilD.txt")
-"""
-omega = force[1]
-if omega < 0.5:
-    init[0] = 700
-if omega >= 0.5:
-    init[0] = 100
-if omega >= 1:
-    init[0] = 40
-"""
+#teilD(init, force, "teilD.txt")
 #init[0] = 65/force[1]
-#res = verlet(init, force)
+#res = verlet(initV2, force)
 #print(verletMax(init, force))
-"""
+
+#res = callVerletMax(init, force)
+#plotting(res, 'Testing')
+
 res = callVerletMax(init, force)
 res1 = callVerletMax(initV1, force)
 res2 = callVerletMax(initV2, force)
-res3 = callVerletMax(initV3, force)
-"""
-"""
-plt.plot(res[0], res[1], label='d=0.4')
-plt.plot(res1[0], res1[1], label='d=0.6')
-plt.plot(res2[0], res2[1], label='d=0.8')
-plt.plot(res3[0], res3[1], label='d=1')
 
 
+plt.plot(res[0], res[1], label='d=3')
+plt.plot(res1[0], res1[1], label='d=1')
+plt.plot(res2[0], res2[1], label='d=3')
+
+
+plt.title('Resonanzkurven bei d >= 1')
 plt.xlabel('omega_A/omega_0')
 plt.ylabel('x_max/x_0')
 plt.grid()
 plt.legend()
 
 plt.show()
-"""
-#Bedeutung der Werte nach Reihenfolge: Laufzeit, Schrittweite, Omega, Startwert x, Startwert v, Dämpfung d
-#Mit anregender kraft. Werte nach Reihenfolge: Amplitude A, Frequenz F
