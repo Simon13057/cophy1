2a30f67bba26a378bb1a0b9af928fca6fb6fb104
Simon Thamm <simon.thamm@uni-konstanz.de>
2021-08-18 10:38:34 +0200

    changed main file for command line arguments

--- swing-by/main.cpp
+++ swing-by/main.cpp
@@ -7,10 +7,27 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    Object first(0., 0., 0., 1., 100.);
-    Object second(1., 0., 5., 10., 10);
+    Object first(0., 0., 5., 0., 10000.);
+    Object second(15., 0., 0., 30., 10);
+
+    if (argc == 11)
+    {
+        first.x = atof(argv[1]);
+        first.y = atof(argv[2]);
+        first.vx = atof(argv[3]);
+        first.vy = atof(argv[4]);
+        double tempMass = atof(argv[5]);
+        first.set_mass(tempMass);
+
+        second.x = atof(argv[6]);
+        second.y = atof(argv[7]);
+        second.vx = atof(argv[8]);
+        second.vy = atof(argv[9]);
+        tempMass = atof(argv[10]);
+        second.set_mass(tempMass);
+    }
 
     LeapFrog sol(30.0, 0.001, "some_data.csv");
     sol.leapfrog_2obj(first, second);
